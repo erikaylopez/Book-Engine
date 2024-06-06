2a72d4ec08ca75505fa4c687c7952455d12cab5d
erikaylopez <eylopez8686@gmail.com>
2024-06-06 14:20:22 -0600

    defined expressMiddleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { ApolloServer } = require('@apollo/server');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
